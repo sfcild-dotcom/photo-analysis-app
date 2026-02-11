14168baa6a0c74be311d69f84a3f0c941e9f669f
sfcild-dotcom <sfcild@gmail.com>
2026-02-11 12:56:59 +0900

    App.tsx
    App.tsx

---
+++ App.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>띄부띄부 사진 분석기가 준비되었습니다!</h1>
+      <p>이제 사진 14장을 업로드하여 분석을 시작해 보세요.</p>
+    </div>
+  );
+}
+
+export default App;
