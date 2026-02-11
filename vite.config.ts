5f277c377d2ebc77ccb7bbfdcb2f0d71bee2cd47
sfcild-dotcom <sfcild@gmail.com>
2026-02-11 12:59:49 +0900

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': {}
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   }
 });
